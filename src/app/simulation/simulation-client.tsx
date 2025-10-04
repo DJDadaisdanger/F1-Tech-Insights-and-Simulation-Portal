43d540daef15b4328613d8ece7e60b1aee7aa04d
DJDada Dangerous <djdadaisdanger@gmail.com>
2025-10-04 13:37:40 +0000

    Try fixing this error: `Console Error: Received `true` for a non-boolean

--- src/app/simulation/simulation-client.tsx
+++ src/app/simulation/simulation-client.tsx
@@ -6,20 +6,20 @@ export function SimulationClient() {
     <iframe 
       title="F1 car" 
       frameBorder="0" 
-      allowFullScreen 
+      allowFullScreen
       //@ts-ignore
       mozallowfullscreen="true" 
       //@ts-ignore
       webkitallowfullscreen="true" 
       allow="autoplay; fullscreen; xr-spatial-tracking" 
       //@ts-ignore
-      xr-spatial-tracking 
+      xr-spatial-tracking="true"
       //@ts-ignore
-      execution-while-out-of-viewport 
+      execution-while-out-of-viewport="true"
       //@ts-ignore
-      execution-while-not-rendered 
+      execution-while-not-rendered="true"
       //@ts-ignore
-      web-share 
+      web-share="true"
       src="https://sketchfab.com/models/bb6d1df708d945f6b12e506e0d39de97/embed"
       className="w-full h-full"
     >
